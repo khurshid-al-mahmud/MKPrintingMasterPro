f96351ce6ef24d3e414b51ba0608807890ab1c4b
Khurshid Al Mahmud <mkgp.raj.bd@gmail.com>
2026-07-15 22:49:04 +0600

    Update Alembic env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,35 +1,24 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
 from app.core.config import settings
 from app.database.engine import Base
 
-# Alembic Config object
 config = context.config
 
-# Database URL from .env via config.py
 config.set_main_option("sqlalchemy.url", settings.database_url)
 
-# Logging
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
-
-# Import all models here
-# Example:
-# from app.models.customer import Customer
 
 target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
-    """Run migrations in offline mode."""
-
-    url = config.get_main_option("sqlalchemy.url")
-
     context.configure(
-        url=url,
+        url=settings.database_url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -41,16 +30,18 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in online mode."""
+    configuration = config.get_section(config.config_ini_section)
+
+    if configuration is None:
+        raise RuntimeError("Alembic configuration not found.")
 
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
+        configuration,
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
-
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
