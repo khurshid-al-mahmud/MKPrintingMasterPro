c9881cbea3db095882cd9413ad7f047781135d1f
Khurshid Al Mahmud <mkgp.raj.bd@gmail.com>
2026-07-21 16:13:48 +0600

    Build-009: Prepare Paper Brand Module

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,33 @@
-from app.models.party import Party
+from app.models.binding_type import BindingType
 from app.models.customer_profile import CustomerProfile
-from app.models.supplier_profile import SupplierProfile
 from app.models.employee_profile import EmployeeProfile
-from app.models.print_partner_profile import PrintPartnerProfile
+from app.models.machine import Machine
+from app.models.number_sequence import NumberSequence
+from app.models.paper_brand import PaperBrand
+from app.models.paper_gsm import PaperGSM
+from app.models.paper_size import PaperSize
+from app.models.paper_type import PaperType
+from app.models.party import Party
 from app.models.party_contact import PartyContact
 from app.models.party_role import PartyRole
-from app.models.number_sequence import NumberSequence
+from app.models.print_partner_profile import PrintPartnerProfile
+from app.models.product import Product
+from app.models.supplier_profile import SupplierProfile
 
-from app.models.paper_size import PaperSize
+__all__ = [
+    "BindingType",
+    "CustomerProfile",
+    "EmployeeProfile",
+    "Machine",
+    "NumberSequence",
+    "PaperBrand",
+    "PaperGSM",
+    "PaperSize",
+    "PaperType",
+    "Party",
+    "PartyContact",
+    "PartyRole",
+    "PrintPartnerProfile",
+    "Product",
+    "SupplierProfile",
+]
